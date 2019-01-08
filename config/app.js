7db4d52b9a734fc147df2726417a73853f4f4d68
Oleg Shikov <ol.shikov@yandex.ru>
2019-01-08 20:25:43 +0300

    fix(Core): convert APP_DEBUG to boolean value for correct work

--- config/app.js
+++ config/app.js
@@ -1,5 +1,10 @@
 const env = require("./env");
 
 module.exports = {
-    debug: env("APP_DEBUG", false),
+  debug: convertToBool(env("APP_DEBUG", false)),
 };
+
+function convertToBool(value) {
+  value = value.trim();
+  return !(value === "false" || value === 0 || value === false);
+}
