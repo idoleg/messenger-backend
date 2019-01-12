00122ceeed1f8a833802e8c1c783945143537a0a
Igor <superpike@mail.ru>
2019-01-13 03:08:26 +0500

    change setup configs (port + host), add new packages

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
+const env = require('./env');
+
 module.exports = {
-  port: 80,
-  host: "localhost",
+  port: env("HTTP_SERVER_PORT"),
+  host: env("HTTP_SERVER_HOST"),
   cors: {
     methods: ["GET", "PUT", "POST", "DELETE", "HEAD", "PATH", "LINK", "UNLINK"],
   },
