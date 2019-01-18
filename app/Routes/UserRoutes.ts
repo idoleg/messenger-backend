8ac9e7afa8e2cad0f5e2ea0d5d41e02196f7db73
Yahor <Yahor.Padhaiski@itechart-group.com>
2019-01-18 11:52:29 +0300

    Refactor changes https://trello.com/c/amhc7AP8/7-%D0%BF%D0%BE%D0%B8%D1%81%D0%BA-%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8F

--- app/Routes/UserRoutes.ts
+++ app/Routes/UserRoutes.ts
@@ -1,13 +1,13 @@
 import {Express} from "express";
 import MessageController from "../Controllers/User/MessageController";
-import GetUserController from "../Controllers/User/UserContoller";
+import UserController from "../Controllers/User/UserContoller";
 
 export default function(express: Express) {
 
     this.get("/users/:userId/messages", MessageController.getCollection);
     this.get("/users/:userId/messages/:messageId", MessageController.getOne);
     this.post("/users/:userId/messages", MessageController.send);
-    this.get("/users", GetUserController.getByEmail);
-    this.get("/users/:userId", GetUserController.getUserById);
+    this.get("/users", UserController.getByEmail);
+    this.get("/users/:userId", UserController.getUserById);
 
 }
