bc495f774d14185be3eecfa471c71956a6008278
Igor <superpike@mail.ru>
2019-01-26 17:40:00 +0500

    move token check to user model

--- app/Middleware/CheckAuth.ts
+++ app/Middleware/CheckAuth.ts
@@ -9,31 +9,14 @@ const User = DB.getModel<IUser, IUserModel>("User");
 export default class CheckAuth {
     public static async isAuth(req: any, res: any, next: any) {
         try {
-            let user: Document | null;
             if (req.method === "OPTIONS" || req.url === "/auth/login" || req.url === "/auth/registration" || req.url === "/socket") {
                 return next();
             }
             const authHeader = req.get("Authorization");
-            if (!authHeader || authHeader.split(" ")[0] !== "Bearer") {
-                throw new httpError.Unauthorized("No valid token");
-            }
-            const token = authHeader.split(" ")[1];
-            const decodedToken = jwt.verify(token, Config.get("auth.privateKey")) as any;
-
-            if (!decodedToken || typeof  decodedToken !== "object") {
-                throw new httpError.Unauthorized("No valid token");
-            }
-
-            if ("userId" in decodedToken) {
-                user = await User.findById(decodedToken.userId);
-            } else {
-                throw new httpError.Unauthorized("No valid token");
-            }
-
+            let user = await User.checkToken(authHeader);
             if (!user) {
-                throw new httpError.Unauthorized("No valid token");
-            }
-
+                throw new httpError.Unauthorized("No valid token")
+            };
             req.user = user;
             next();
         } catch (err) {
--- app/Models/User.ts
+++ app/Models/User.ts
@@ -2,6 +2,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import {Mongoose, Schema} from "mongoose";
 import {Config} from "../index";
+import {Document} from "mongoose";
 
 const UserSchema = new Schema({
     email: {type: String, index: true, unique: true, required: true, trim: true},
@@ -36,6 +37,30 @@ UserSchema.method("createToken", function() {
         Config.get("auth.privateKey"),
         {expiresIn: Config.get("auth.expiresTime")},
     );
+});
+
+UserSchema.static("checkToken", async function(authHeader: string) {
+    if (!authHeader || authHeader.split(" ")[0] !== "Bearer") {
+        return null;
+    }
+    const token = authHeader.split(" ")[1];
+    const decodedToken = jwt.verify(token, Config.get("auth.privateKey")) as any;
+    let user: Document | null;
+
+    if (!decodedToken || typeof  decodedToken !== "object") {
+        return null;
+    }
+
+    if ("userId" in decodedToken) {
+        user = await this.findById(decodedToken.userId);
+    } else {
+        return null;
+    }
+
+    if (!user) {
+        return null;
+    }
+    return user;
 });
 
 export default (mongoose: Mongoose) => {
